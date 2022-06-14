ee708b047fb92aa7a67eb762c305f2cdd6b20ac4
Xuanmin Mai <maix@miamioh.edu>
2022-06-14 16:36:47 -0700

    ui:fix

--- EmotionRecognition-FunTime/EmotionRecognition-FunTime/ClientApp/proxy.conf.js
+++ EmotionRecognition-FunTime/EmotionRecognition-FunTime/ClientApp/proxy.conf.js
@@ -6,7 +6,10 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/textanalysis"
+      "/textanalysis",
+      "/weatherforecast",
+      "/Knowledgebase",
+      "/KnowledgebaseAPI"
    ],
     target: target,
     secure: false
