0f5ac2e9994e75f9f14ce8adb77d995b276b79a0
Hakony <anphamhkn@gmail.com>
2022-06-14 16:30:18 -0700

    update text analytics and add user controller

---
+++ EmotionRecognition-FunTime/EmotionRecognition-FunTime/Migrations/20220614021441_InitDatabase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmotionRecognition_FunTime.Migrations
+{
+    public partial class InitDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TextAnalyticModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    reason = table.Column<int>(type: "int", nullable: true),
+                    time = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextAnalyticModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionAnalyticsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FollowedUpQuestion = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionUsers_TextAnalyticModel_QuestionAnalyticsId",
+                        column: x => x.QuestionAnalyticsId,
+                        principalTable: "TextAnalyticModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionUsers_QuestionAnalyticsId",
+                table: "QuestionUsers",
+                column: "QuestionAnalyticsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionUsers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "TextAnalyticModel");
+        }
+    }
+}
